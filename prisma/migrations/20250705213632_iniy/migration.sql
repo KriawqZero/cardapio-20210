d237ea51ad17114823b68b79683ec86d71e5947f
Marcilio Ortiz <supergamerbr39@gmail.com>
2025-08-06 10:56:04 -0400

    Delete prisma/migrations directory

--- prisma/migrations/20250705213632_iniy/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE `drinks` ADD COLUMN `esgotado` BOOLEAN NOT NULL DEFAULT false,
-    ADD COLUMN `esgotando` BOOLEAN NOT NULL DEFAULT false,
-    ADD COLUMN `novidade` BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE `pedidos` ADD COLUMN `observacao` VARCHAR(191) NULL;
